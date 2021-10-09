f37013a91abbaa6fbf40781698fc96e66a5eef4f
Kriti Jain <76650361+kritij19@users.noreply.github.com>
2021-10-09 12:32:33 +0530

    Update filters.c

--- Filters/filters.c
+++ Filters/filters.c
@@ -1,7 +1,6 @@
 # include <math.h>
 
 // GRAYSCALE: 
-
 // Calculate the avg of r,g,b values and put all values equal to avg
 // **x/y returns an int. To get a float return value (float)x/y or x.0/y or x/y.0
 // round() always rounds up
@@ -25,7 +24,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 // SEPIA : 
-
 // Given formulas for conversions
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -36,6 +34,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int originalRed = image[i][j].rgbtRed;
             int originalBlue = image[i][j].rgbtBlue;
             int originalGreen = image[i][j].rgbtGreen;
+            // Getting sepia values of rgb
             int sepia_red = (int) round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
             int sepia_green = (int) round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
             int sepia_blue = (int) round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
@@ -61,7 +60,6 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 }
 
 // REFLECT IMAGE HORIZONTALLY:
-
 // Replacing value of pixel in the front with the corresponding pixel at the end using a temporary variable
 // If there is a middle row it needs to stay unchanged
 // Middle row will exist in case of odd number of rows
@@ -87,7 +85,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 }
 
 // BLUR IMAGE:
-
 // We take avg of r,g,b values in the respective array to be considered
 // For corners: 2x2 array
 // For edges: 2x3 /3x2 array
@@ -111,7 +108,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             int sum_red = 0;
             int sum_blue = 0;
             int sum_green = 0;
-            //In case of a corner 4 cases
+            // In case of a corner- 4 cases
             // 2x2 array array considered
             if ((i == 0 && j == 0) || (i == 0 && j == width - 1) || (i == height - 1 && j == 0) || (i == height - 1 && j == width - 1))
             {
@@ -280,11 +277,11 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            //If the pixel lies one of the edges
-            //We will consider all the pixels in the the 3 x 3 grid for corners/edges which do not lie in the picture to be black
+            // If the pixel lies one of the edges
+            // We will consider all the pixels in the the 3 x 3 grid for corners/edges which do not lie in the picture to be black
             if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
             {
-                //The array keeps track of the values r,g,b values of the 3 x 3 array
+                // The array keeps track of the values r,g,b values of the 3 x 3 array
                 int arr_red[9];
                 int arr_blue[9];
                 int arr_green[9];
@@ -309,7 +306,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                         }
                     }
                 }
-                //gx and gy are calculted for r,g,b.
+                // gx and gy are calculted for r,g,b.
                 // For each of the pixel the calculation is done 6 times. 3 times for gx and 3 times for gy.
                 int red_gx = -1 * arr_red[0] + arr_red[2] + -2 * arr_red[3] + 2 * arr_red[5] + -1 * arr_red[6] + arr_red[8];
                 int blue_gx = -1 * arr_blue[0] + arr_blue[2] + -2 * arr_blue[3] + 2 * arr_blue[5] + -1 * arr_blue[6] + arr_blue[8];
